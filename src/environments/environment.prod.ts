7b5860a692985f32e39451e0c0d77b487afcbb93
skor12 <76225114+skor12@users.noreply.github.com>
2022-06-06 11:21:24 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://6eqbpce6ya.execute-api.us-east-2.amazonaws.com'
 };
